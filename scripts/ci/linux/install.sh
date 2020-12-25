efaa95995f4c0c54e27a22843131c09273aaf240
Thomas Geissl <thomas.geissl@gmail.com>
2020-12-25 02:37:44 +0100

    building newest cmake version on linux

--- scripts/ci/linux/install.sh
+++ scripts/ci/linux/install.sh
@@ -1,2 +1,6 @@
-sudo apt update
-sudo apt upgrade cmake
+# sudo apt update
+# sudo apt upgrade cmake
+
+git clone https://github.com/Kitware/CMake.git
+cd CMake
+./bootstrap && make && sudo make install
