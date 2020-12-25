e801bef951b64f4e8210692f7b35dbfcef7665be
Thomas Geissl <thomas.geissl@gmail.com>
2020-12-25 01:55:31 +0100

    installing qt6

---
+++ scripts/ci/common/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -ev
+
+git clone --recursive https://github.com/qt/qtbase.git
+cd qtbase
+mkdir build && cd build
+cmake .. -G Ninja
+cmake --build . --parallel
+sudo cmake --install .
--- scripts/ci/osx/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -ev
-
-brew install qt5 
-brew link --force qt5
